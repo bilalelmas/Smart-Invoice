1a2e0c0f7d27139ad4a6498ea9dad5686bd8bc49
Bilal Elmas <bilaelmas@gmail.com>
2025-12-01 12:20:25 +0300

    Initial Commit

---
+++ Smart InvoiceTests/Smart_InvoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_InvoiceTests.swift
+//  Smart InvoiceTests
+//
+//  Created by Bilal Elmas on 1.12.2025.
+//
+
+import Testing
+@testable import Smart_Invoice
+
+struct Smart_InvoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
